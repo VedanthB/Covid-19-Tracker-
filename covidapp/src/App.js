7ac660e18c9dadcd7580156f2fe407315dba5113
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-04-28 02:21:57 +0530

    ADD: header with the map function to map countries on dropdown

--- covidapp/src/App.js
+++ covidapp/src/App.js
@@ -1,26 +1,60 @@
-
 import './App.css';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import { useEffect, useState } from 'react';
 
+// https://disease.sh/v3/covid-19/countries
 
 function App() {
+  const [countries, setCountries] = useState([])
+  const [country, setCountry] = useState('worldwide')
+
+
+  useEffect(() => {
+      // async -> send a requesst, wait for it, do something(get the countries and map them tin the list)
+      const getContriesData = async () => {
+         await fetch('https://disease.sh/v3/covid-19/countries')
+          .then((response) => response.json())
+          .then((data) => {
+               const countries = data.map((country) => (
+                 {
+                 name: country.country,
+                 value: country.countryInfo.iso2
+               }
+               ))
+               setCountries(countries)
+           }
+           )
+      }
+      getContriesData();
+  }, []) 
   
+  const onCountryChange =  (event) => {
+    const countryCode = event.target.value
+
+    setCountry(countryCode)
+  }
+
 
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className='app__dropdown'>
-            <Select variant='outlined' value='123' >
+            <Select 
+             variant='outlined' 
+             value={country} 
+             onChange={onCountryChange}
+             >
 
-              {/* Loop through the countries and then show dropdown list of them */}
-
-
+            {/* Loop through the countries and then show dropdown list of them */}
             <MenuItem value='worldwide'>Worldwide</MenuItem>
-            <MenuItem value='worldwide'>Worldwide</MenuItem>
-            <MenuItem value='worldwide'>Worldwide</MenuItem>
+             {
+               countries.map((country) => 
+                  <MenuItem value={country.value} > {country.name} </MenuItem> )
+             }   
+           
            </Select> 
         </FormControl>
       </div>
